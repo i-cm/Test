51e9b6c63188d0b9fd84d2b0a44c835c77fe5e58
i-cm <74533816+i-cm@users.noreply.github.com>
2024-01-20 18:02:52 +1100

    Create Wmod.js

---
+++ Wmod.js
@@ -0,0 +1,23 @@
+behaviors.CRASH = [
+    "XX|XX|XX",
+    "XX|CH|XX",
+    "XX|XX|XX",
+];
+
+elements.crash = {
+    color: ["#ff0000", "#000000",],
+    behavior: behaviors.CRASH,
+    category: "bruh",
+    viscosity: 100000,
+    state: "liquid",
+    density: 720,
+};
+
+elements.darkmatter = {
+    color: "#000000",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    viscosity: 100000,
+    state: "special",
+    density: 720,
+};
